d58694bef9da4bc2eff490c2aab32f1234c00630
Yuvraj Singh <131570406+yuvrajsingh08@users.noreply.github.com>
2025-09-04 12:35:57 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
 
         <div className="mt-28 md:mt-5 lg:5">
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className="text-[#915EFF]">surya</span>
+            Hi, I'm <span className="text-[#915EFF]">Surya</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
            
